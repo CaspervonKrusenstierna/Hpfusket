c71776b3a287fd7e34c3e0bd2aa8f82d6b1d8bf6
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-01-25 22:42:52 +0100

    commit

--- frontend/DashboardPublic/src/pages/paymentpage/PaymentPage.jsx
+++ frontend/DashboardPublic/src/pages/paymentpage/PaymentPage.jsx
@@ -16,10 +16,10 @@ const PaymentPage = (props) => {
             <div className='PaymentPage'>
               <div className='Title'>Priser</div>
               <div className='PricingBoxContainer'>
-                <PricingBox onClick={onBuyClick} title="1.4+" price="14000kr" id={1}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.6+" price="17500kr" id={2}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.8+" price="20000kr" id={3}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.95+" price="25000kr" id={4}></PricingBox>
+                <PricingBox onClick={onBuyClick} title="1.35+" price="25000kr" id={1}></PricingBox>
+                <PricingBox onClick={onBuyClick} title="1.5+" price="45000kr" id={2}></PricingBox>
+                <PricingBox onClick={onBuyClick} title="1.75+" price="70000kr" id={3}></PricingBox>
+                <PricingBox onClick={onBuyClick} title="1.95+" price="95000kr" id={4}></PricingBox>
               </div>
             <PaymentBox id={id} onExit={RenderStandardPage}></PaymentBox>
         </div>
@@ -33,12 +33,12 @@ const PaymentPage = (props) => {
               <div className='Title'>Priser</div>
               <div className='PricingBoxContainer'>
                 <div className='PricingBoxSubContainer'>
-                <PricingBox onClick={onBuyClick} title="1.4+" price="14000kr" id={1}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.6+" price="17500kr" id={2}></PricingBox>
+                <PricingBox onClick={onBuyClick} title="1.35+" price="25000kr" id={1}></PricingBox>
+                <PricingBox onClick={onBuyClick} title="1.5+" price="45000kr" id={2}></PricingBox>
                 </div>
                 <div className='PricingBoxSubContainer'>
-                  <PricingBox onClick={onBuyClick} title="1.8+" price="20000kr" id={3}></PricingBox>
-                  <PricingBox onClick={onBuyClick} title="1.95+" price="25000kr" id={4}></PricingBox>
+                  <PricingBox onClick={onBuyClick} title="1.75+" price="70000kr" id={3}></PricingBox>
+                  <PricingBox onClick={onBuyClick} title="1.95+" price="95000kr" id={4}></PricingBox>
                 </div>
               </div>
         </div>
