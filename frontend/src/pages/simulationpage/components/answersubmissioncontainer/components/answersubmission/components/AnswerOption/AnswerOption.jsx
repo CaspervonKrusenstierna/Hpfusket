89c1b49d5ad5f99bf4fca25d7a36b39f4b2e2e4f
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-05-26 18:37:05 +0200

    Update AnswerOption.jsx

--- frontend/src/pages/simulationpage/components/answersubmissioncontainer/components/answersubmission/components/AnswerOption/AnswerOption.jsx
+++ frontend/src/pages/simulationpage/components/answersubmissioncontainer/components/answersubmission/components/AnswerOption/AnswerOption.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import "./AnswerOption.css"
 
 const AnswerOption = (props) => {
-  if(props.isSelected){
-    return (
-      <button className='AnswerOptionSelected' onClick={props.onClick}>
-        {props.Text}
-      </button>
-    )
-  }
+
   return (
-    <button className='AnswerOptionUnSelected' onClick={props.onClick}>
+    <button className={props.isSelected ? 'AnswerOptionSelected' : 'AnswerOptionUnSelected'} onClick={props.onClick}>
       {props.Text}
     </button>
   )
