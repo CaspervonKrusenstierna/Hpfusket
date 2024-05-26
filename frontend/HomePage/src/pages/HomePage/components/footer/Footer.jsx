c9c310bd6a518385407f73453e806bd4557a1861
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-05-26 14:26:35 +0200

    Update Footer.jsx

--- frontend/HomePage/src/pages/HomePage/components/footer/Footer.jsx
+++ frontend/HomePage/src/pages/HomePage/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='Footer'>
         <div className='FooterWrapper'><h1>Kontakt: Hpfusk@proton.me</h1></div>
         <div className='FooterWrapper'><p>Copyright © 2023 HPFUSK. All rights reserved. <br />
-All other trademarks, screenshots, logos and copyrights are the property of their respective owners.</p></div>
+        All other trademarks, screenshots, logos and copyrights are the property of their respective owners.</p></div>
     </div>
   )
 }
