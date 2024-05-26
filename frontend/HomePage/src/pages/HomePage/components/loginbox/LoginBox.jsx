677a4a0e4888b490f31710ae855f02219155c624
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-05-26 14:11:35 +0200

    Update LoginBox.jsx

--- frontend/HomePage/src/pages/HomePage/components/loginbox/LoginBox.jsx
+++ frontend/HomePage/src/pages/HomePage/components/loginbox/LoginBox.jsx
@@ -1,61 +1,30 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./LoginBox.css"
 import {Input, SubmitButton, ErrorMessage, CheckBoxButton, AnchorButton, ExitButton} from "../../../common/components"
 import {UserImg, UnlockImg} from  "../../../common/assets"
-
-let Username = "";
-let Password = "";
-let RememberMe = false;
+import Login from './Login'
 
 const LoginBox = (props) => {
     const [error, setError] = useState();
-  
+    const loginInfo = useRef({Username: "", Password: "", RememberMe: ""})
+
     async function onSubmit(){
-      const response = await fetch("http://127.0.0.1:3000/login", {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify({"name": Username, "password": Password, "remember": RememberMe})
-      }).then((res) => res.json());
-  
+      let currLoginInfo = loginInfo.current;
+      let response = await Login(currLoginInfo.Username, currLoginInfo.Password, currLoginInfo.RememberMe);
+      
       if(response.success){
         window.location.href = props.redirect;
       }
       setError(response.message);
     }
 
-    if(Error !== ""){
-      return (
-        <div className='LoginBox'>
-            <div className='ExitButtonContainer'>
-              <ExitButton onClick={props.onExitClick}></ExitButton>
-            </div>
-            <div className='inputcontainer-login'>
-              <ErrorMessage message={error}></ErrorMessage>
-              <Input onChange={(e) => {Username = e.target.value}} label="Användarnamn" img={UserImg}></Input>
-              <Input hide={true} onChange={(e) => {Password = e.target.value}} label="Lösenord" img={UnlockImg}></Input>
-              <div className='LoginBox-BottomContainer'>
-                <CheckBoxButton onChange={(e) => {}} text="Kom ihåg mig"></CheckBoxButton>
-                <AnchorButton text="Glömt ditt lösenord?" onClick={props.onForgotPassword}></AnchorButton>
-              </div>
-              <SubmitButton onClick={onSubmit} text="Logga in"></SubmitButton>
-            </div>
-        </div>
-      )
-    }
-  
     return (
       <div className='LoginBox'>
           <div className='ExitButtonContainer'>
               <ExitButton onClick={props.onExitClick}></ExitButton>
           </div>
           <div className='inputcontainer-login'>
+            {error ? <ErrorMessage message={error}></ErrorMessage> : <></>}
             <Input onChange={(e) => {Username = e.target.value}} label="Användarnamn" img={UserImg}></Input>
             <Input hide={true} onChange={(e) => {Password = e.target.value}} label="Lösenord" img={UnlockImg}></Input>
             <div className='LoginBox-BottomContainer'>
