2816db2c0cd90a25da1787059612ed0c0cf7bdb2
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-05-26 14:18:17 +0200

    Update ForgotPasswordBox.jsx

--- frontend/HomePage/src/pages/HomePage/components/forgotpasswordbox/ForgotPasswordBox.jsx
+++ frontend/HomePage/src/pages/HomePage/components/forgotpasswordbox/ForgotPasswordBox.jsx
@@ -1,51 +1,26 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {Input, SubmitButton, ErrorMessage, ExitButton, VerificationMessage} from "../../../common/components"
 import {EnvelopeImg} from  "../../../common/assets"
 import "./ForgotPasswordBox.css"
-
-let Email = "";
+import ForgotPassword from './ForgotPassword';
 
 const ForgotPasswordBox = (props) => {
     const [hasSubmitted, setHasSubmitted] = useState(false);
+    const email = useRef("");
 
     async function onSubmit(){
-      const response = await fetch("http://127.0.0.1:3000/forgotpassword", {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify({"email": Email})
-      }).then((res) => res.json());
-  
-
+      const response = await ForgotPassword(email.current)
       setHasSubmitted(true);
     }
-    if(hasSubmitted){
-        return (
-            <div className='ForgotPasswordBox'>
-                <div className='ExitButtonContainer'>
-                  <ExitButton onClick={props.onExitClick}></ExitButton>
-                </div>
-                <div className='inputcontainer-forgotpassword'>
-                  <VerificationMessage text="Gå in på din email för vidare instruktioner."></VerificationMessage>
-                  <Input onChange={(e) => {Email = e.target.value}} label="Email" img={EnvelopeImg}></Input>
-                  <SubmitButton onClick={onSubmit} text="Bekräfta"></SubmitButton>
-                </div>
-            </div>
-          );
-    }
+
     return (
         <div className='ForgotPasswordBox'>
             <div className='ExitButtonContainer'>
               <ExitButton onClick={props.onExitClick}></ExitButton>
             </div>
             <div className='inputcontainer-forgotpassword'>
-              <Input onChange={(e) => {Email = e.target.value}} label="Email" img={EnvelopeImg}></Input>
+              {hasSubmitted ? <VerificationMessage text="Gå in på din e-post adress för vidare instruktioner."></VerificationMessage> : <></>}
+              <Input onChange={(e) => {email.current = e.target.value}} label="Email" img={EnvelopeImg}></Input>
               <SubmitButton onClick={onSubmit} text="Bekräfta"></SubmitButton>
             </div>
         </div>
