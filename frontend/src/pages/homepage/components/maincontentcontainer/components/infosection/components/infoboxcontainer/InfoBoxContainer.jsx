60b3290c3b02f2739854e7a33567c50edc4707ed
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-05-26 21:27:56 +0200

    Update InfoBoxContainer.jsx

--- frontend/src/pages/homepage/components/maincontentcontainer/components/infosection/components/infoboxcontainer/InfoBoxContainer.jsx
+++ frontend/src/pages/homepage/components/maincontentcontainer/components/infosection/components/infoboxcontainer/InfoBoxContainer.jsx
@@ -5,9 +5,9 @@ import { StarIcon, ShieldIcon, CheckMarkIcon } from './icons'
 const InfoBoxContainer = () => {
   return (
     <div className='InfoBoxContainer'>
-    <InfoBox title="Pålitlig" text="Vi har redan många nöjda kunder!"imgsrc={StarIcon}></InfoBox>
-    <InfoBox title="Enkel" text="Det har aldrig varit enklare att komma in på sin drömutbildning! " imgsrc={CheckMarkIcon}></InfoBox>
-    <InfoBox title="Säker" text="Våra kunders säkerhet är oerhört viktigt för oss och du behöver inte lämna ut några personuppgifter för att använda dig av vår tjänst."imgsrc={ShieldIcon}></InfoBox>
+      <InfoBox title="Pålitlig" text="Vi har redan många nöjda kunder!"imgsrc={StarIcon}></InfoBox>
+      <InfoBox title="Enkel" text="Det har aldrig varit enklare att komma in på sin drömutbildning! " imgsrc={CheckMarkIcon}></InfoBox>
+      <InfoBox title="Säker" text="Våra kunders säkerhet är oerhört viktigt för oss och du behöver inte lämna ut några personuppgifter för att använda dig av vår tjänst."imgsrc={ShieldIcon}></InfoBox>
     </div>
   )
 }
