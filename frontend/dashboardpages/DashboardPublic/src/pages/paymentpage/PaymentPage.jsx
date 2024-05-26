6e6a053924e849cb672734590abb8af22ea829bb
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-05-26 14:26:33 +0200

    Update PaymentPage.jsx

--- frontend/dashboardpages/DashboardPublic/src/pages/paymentpage/PaymentPage.jsx
+++ frontend/dashboardpages/DashboardPublic/src/pages/paymentpage/PaymentPage.jsx
@@ -7,47 +7,32 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 
 const PaymentPage = (props) => {
-  const [content, setContent] = useState(<></>);
-  useEffect(() => {
-    function onBuyClick(id){
-      setContent(
-        <div className='unfocus'>
-          <div className='DarknessEffect'></div>
-            <div className='PaymentPage'>
-              <div className='Title'>Priser</div>
-              <div className='PricingBoxContainer'>
-                <PricingBox onClick={onBuyClick} title="1.35+" price="25000kr" id={1}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.5+" price="45000kr" id={2}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.75+" price="70000kr" id={3}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.95+" price="95000kr" id={4}></PricingBox>
-              </div>
-            <PaymentBox id={id} onExit={RenderStandardPage}></PaymentBox>
+  const [renderPaymentBox, setRenderPaymentBox] = useState(false);
+
+  function onBuyClick(){
+    setRenderPaymentBox(true);
+  }
+  function onPaymentBoxExit(){
+    setRenderPaymentBox(false);
+  }
+  let content = (
+    <>
+      <div className='DarknessEffect'></div>
+      <div className='PaymentPage'>
+        <div className='Title'>Priser</div>
+        <div className='PricingBoxContainer'>
+          <PricingBox onClick={onBuyClick} title="1.35+" price="25000kr" id={1}></PricingBox>
+          <PricingBox onClick={onBuyClick} title="1.5+" price="45000kr" id={2}></PricingBox>
+          <PricingBox onClick={onBuyClick} title="1.75+" price="70000kr" id={3}></PricingBox>
+          <PricingBox onClick={onBuyClick} title="1.95+" price="95000kr" id={4}></PricingBox>
         </div>
-      </div>
-      )
-    }
-    function RenderStandardPage(){
-      setContent(
-        <>
-          <div className='PaymentPage'>
-              <div className='Title'>Priser</div>
-              <div className='PricingBoxContainer'>
-                <div className='PricingBoxSubContainer'>
-                <PricingBox onClick={onBuyClick} title="1.35+" price="25000kr" id={1}></PricingBox>
-                <PricingBox onClick={onBuyClick} title="1.5+" price="45000kr" id={2}></PricingBox>
-                </div>
-                <div className='PricingBoxSubContainer'>
-                  <PricingBox onClick={onBuyClick} title="1.75+" price="70000kr" id={3}></PricingBox>
-                  <PricingBox onClick={onBuyClick} title="1.95+" price="95000kr" id={4}></PricingBox>
-                </div>
-              </div>
-        </div>
-      </>
-      )
-    }
-    RenderStandardPage();
-  }, [])
-  return (<>{content}</>)
+        {renderPaymentBox ? <PaymentBox id={id} onExit={onPaymentBoxExit}></PaymentBox> : <></>}
+    </div>
+   </>
+  )
+  
+  return renderPaymentBox ? <div className='unfocus'>{content}</div> : <>{content}</>
+  
 }
 
 export default PaymentPage
