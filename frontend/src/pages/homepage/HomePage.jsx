bc320ee95b38b4d91e16e692b8a17996a4778766
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-05-26 21:28:00 +0200

    Update HomePage.jsx

--- frontend/src/pages/homepage/HomePage.jsx
+++ frontend/src/pages/homepage/HomePage.jsx
@@ -6,29 +6,29 @@ const HomePage = (props) => {
   const [toRender, setToRender] = useState(<></>)
 
   useEffect(() => {
-    let headerComponent = <Header loggedIn={props.isLoggedIn} onLoginClick={LoginClick} onRegisterClick={RegisterClick} disableSticky={false}/>
-    let darknessEffect = <div className='DarknessEffect'/>
+      let headerComponent = <Header loggedIn={props.isLoggedIn} onLoginClick={LoginClick} onRegisterClick={RegisterClick} disableSticky={false}/>
+      let darknessEffect = <div className='DarknessEffect'/>
 
-    function OnExit(){
-      setToRender(<>{headerComponent}<MainContentContainer/><Footer/></>);
-    }
+      function OnExit(){
+        setToRender(<>{headerComponent}<MainContentContainer/><Footer/></>);
+      }
 
-    function ForgotPasswordClick(){
-      OnExit();
-      setToRender(<div className='HomePage-unfocus'>{darknessEffect}{headerComponent}<MainContentContainer/><Footer/><ForgotPasswordBox onExitClick={OnExit}/></div>)
-    }
+      function ForgotPasswordClick(){
+        OnExit();
+        setToRender(<div className='HomePage-unfocus'>{darknessEffect}{headerComponent}<MainContentContainer/><Footer/><ForgotPasswordBox onExitClick={OnExit}/></div>)
+      }
 
-    function LoginClick(redirect){
-      window.scrollTo(0,0);
-      setToRender(<div className='HomePage-unfocus'>{darknessEffect}{headerComponent}<MainContentContainer/><Footer/><LoginBox redirect={redirect} onExitClick={OnExit} onForgotPassword={ForgotPasswordClick}/></div>)
-    }
+      function LoginClick(redirect){
+        window.scrollTo(0,0);
+        setToRender(<div className='HomePage-unfocus'>{darknessEffect}{headerComponent}<MainContentContainer/><Footer/><LoginBox redirect={redirect} onExitClick={OnExit} onForgotPassword={ForgotPasswordClick}/></div>)
+      }
 
-    function RegisterClick(){
-      window.scrollTo(0,0);
-      setToRender(<div className='HomePage-unfocus'>{darknessEffect}{headerComponent}<MainContentContainer/><Footer/><RegisterBox onExitClick={OnExit}/></div>)
-    }
-    
-    setToRender(<>{headerComponent}<MainContentContainer/><Footer/></>)
+      function RegisterClick(){
+        window.scrollTo(0,0);
+        setToRender(<div className='HomePage-unfocus'>{darknessEffect}{headerComponent}<MainContentContainer/><Footer/><RegisterBox onExitClick={OnExit}/></div>)
+      }
+      
+      setToRender(<>{headerComponent}<MainContentContainer/><Footer/></>)
   }, [])
 
 
